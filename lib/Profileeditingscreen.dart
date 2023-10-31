7e46de2160d6e4750b3cd5e6650f36dcc9738c8e
Muhammad Saqlain <miansaqlainqureshi@gmail.com>
2023-10-31 05:50:40 -0700

    Update

--- lib/Profileeditingscreen.dart
+++ lib/Profileeditingscreen.dart
@@ -10,6 +10,33 @@ class Profileeditingscreen extends StatefulWidget {
 class _ProfileeditingscreenState extends State<Profileeditingscreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'User Profile',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+      ),
+      body: Column(children: [
+        Row(
+          children: [
+            CircleAvatar(
+              radius: 50,
+              backgroundColor: Colors.green,
+            ),
+            SizedBox(
+              width: 10,
+            ),
+            Column(
+              children: [
+                Container(
+                  child: Text('Name'),
+                )
+              ],
+            )
+          ],
+        )
+      ]),
+    );
   }
 }
