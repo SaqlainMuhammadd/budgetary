54e845927ad3b9c2bd510bcb8751f688bb7649ea
Muhammad Saqlain <miansaqlainqureshi@gmail.com>
2023-11-28 05:16:36 -0800

    HomePage created

---
+++ lib/homepage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class homepage extends StatefulWidget {
+  const homepage({super.key});
+
+  @override
+  State<homepage> createState() => _homepageState();
+}
+
+class _homepageState extends State<homepage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
