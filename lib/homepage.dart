2132f4344350da09f10c3f3faf2b4eade17ba9eb
Muhammad Saqlain <miansaqlainqureshi@gmail.com>
2023-11-29 05:21:18 -0800

    updated

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -10,6 +10,6 @@ class homepage extends StatefulWidget {
 class _homepageState extends State<homepage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold();
   }
 }
