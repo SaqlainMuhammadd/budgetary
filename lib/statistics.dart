336e664380d7a69b5ef70df2d152f1dca4184dbf
MuhammadSaqlain143 <miansaqlainqureshi@gmail.com>
2024-01-05 02:47:04 -0800

    Updated

---
+++ lib/statistics.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class statisticsScreen extends StatefulWidget {
+  const statisticsScreen({super.key});
+
+  @override
+  State<statisticsScreen> createState() => _statisticsScreenState();
+}
+
+class _statisticsScreenState extends State<statisticsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
